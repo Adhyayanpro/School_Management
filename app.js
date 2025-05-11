286e5855e509efb1098539809ecbe14bf0f4a524
Adhyayanpro <adhyayan0520@gmail.com>
2025-05-12 00:48:20 +0530

    testing

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express();
 
 app.use(express.json());
 app.use('/', schoolRoutes);
+app.get('/', (req, res) => {
+  res.send('API is running!');
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
